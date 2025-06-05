9e2c5644ed49410b20f3e86e21463a123942fa0d
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-06-05 16:28:00 +0100

    remove commented out code

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -52,10 +52,8 @@ export default function MainMenu({ user }: MainMenuProps) {
     return (
         <>
             {isMobileBreakpoint ? (
-                // --- Mobile/Drawer View ---
+                // --- Mobile View ---
                 <>
-                    
-
                     <Drawer.Root
                         placement='end'
                         open={isOpen}
@@ -184,9 +182,8 @@ export default function MainMenu({ user }: MainMenuProps) {
                     </Drawer.Root>
                 </>
             ) : (
-                // --- Desktop/Non-Collapsible View (HStack) ---
+                // --- Desktop View ---
                 <HStack colorPalette="teal">
-                    {/* Your existing desktop menu items */}
                     <ChakraLink
                         asChild
                         color={linkColour}
@@ -278,197 +275,5 @@ export default function MainMenu({ user }: MainMenuProps) {
                 </HStack>
             )}
         </>
-
-        /*
-        <>
-            {isCollapsible ? (
-            <Collapsible.Root as="nav" width="max-content" justifyItems="end">
-                <Collapsible.Trigger asChild>
-                <IconButton 
-                    color="teal.solid"
-                    variant="outline"
-                    size="md"
-                    borderColor="teal.solid"
-                    borderWidth={2}
-                    >
-                    <FaBars />
-                </IconButton>
-                </Collapsible.Trigger>
-                <Collapsible.Content>
-                <VStack alignItems="end">
-                    {// Browse all events }
-                    <ChakraLink
-                        asChild
-                        color={linkColour}
-                        textDecorationColor={linkColour}
-                        fontSize="md"
-                        fontWeight="semibold"
-                    >
-                        <NavLink to="/">Browse all events</NavLink>
-                    </ChakraLink>
-
-                    {// Help }
-                    <ChakraLink
-                        asChild
-                        color={linkColour}
-                        textDecorationColor={linkColour}
-                        fontSize="md"
-                        fontWeight="semibold"
-                    >
-                        <NavLink to="/help">Help</NavLink>
-                    </ChakraLink>
-
-                    {// Account }
-                    {user?.user_id && (
-                        <>
-                            <ChakraLink asChild>
-                                <NavLink to="/users/me">
-                                    <HStack>
-                                        <Icon color={linkColour} size="md">
-                                            <FaUserCircle />
-                                        </Icon>
-                                        <Text
-                                            color={linkColour}
-                                            fontSize="md"
-                                            fontWeight="semibold"
-                                        >
-                                            Account
-                                        </Text>
-                                    </HStack>
-                                </NavLink>
-                            </ChakraLink>
-                            {user?.organisation_id && (
-                                <ChakraLink asChild>
-                                    <NavLink to={`/organisations/${user.organisation_id}/events`}>
-                                        <Text
-                                            marginLeft={6}
-                                            color={linkColour}
-                                            fontSize="md"
-                                            fontWeight="semibold"
-                                        >
-                                            Staff Dashboard
-                                        </Text>
-                                    </NavLink>
-                                </ChakraLink>
-                            )}
-                            <ChakraLink asChild>
-                                <Link
-                                    variant="plain"
-                                    textDecoration="none"
-                                    onClick={handleSignOutClick}
-                                >
-                                    <Text
-                                        marginLeft={6}
-                                        color={linkColour}
-                                        fontSize="md"
-                                        fontWeight="semibold"
-                                    >
-                                        Sign Out
-                                    </Text>
-                                </Link>
-                            </ChakraLink>
-                        </>
-                    )}
-                    </VStack>
-                </Collapsible.Content>
-            </Collapsible.Root>
-            ) : (
-
-        <HStack colorPalette="teal">
-            {// Browse all events }
-            <ChakraLink
-                asChild
-                color={linkColour}
-                variant="underline"
-                textDecorationColor={linkColour}
-                fontSize="lg"
-                fontWeight="semibold"
-            >
-                <NavLink to="/">Browse all events</NavLink>
-            </ChakraLink>
-
-            <Text color={linkColour} fontSize="2xl" fontWeight="semibold">
-                |
-            </Text>
-
-            {// Help }
-            <ChakraLink
-                asChild
-                color={linkColour}
-                variant="underline"
-                textDecorationColor={linkColour}
-                fontSize="lg"
-                fontWeight="semibold"
-            >
-                <NavLink to="/help">Help</NavLink>
-            </ChakraLink>
-            <Text color={linkColour} fontSize="2xl" fontWeight="semibold">
-                |
-            </Text>
-
-            {// Account }
-            {user?.user_id && (
-                <>
-                    <Icon color={linkColour} size="lg">
-                        <FaUserCircle />
-                    </Icon>
-                    <Menu.Root positioning={{ placement: 'bottom-end' }}>
-                        <Menu.Trigger>
-                            <Text
-                                color={linkColour}
-                                fontSize="lg"
-                                fontWeight="semibold"
-                            >
-                                Account
-                            </Text>
-                        </Menu.Trigger>
-                        <Portal>
-                            <Menu.Positioner>
-                                <Menu.Content>
-                                    <Menu.Item 
-                                        asChild
-                                        value="profile"
-                                        fontSize="lg"
-                                        fontWeight="semibold"
-                                    >
-                                        <NavLink to="/users/me">Profile</NavLink>
-                                    </Menu.Item>
-                                    {user?.organisation_id && (
-                                        <Menu.Item 
-                                            asChild
-                                            value="staff-dashboard"
-                                            fontSize="lg"
-                                            fontWeight="semibold"
-                                        >
-                                            <NavLink to={`/organisations/${user.organisation_id}/events`}>
-                                                Staff Dashboard
-                                            </NavLink>
-                                        </Menu.Item>
-                                    )}
-                                    <Menu.Separator />
-                                    <Menu.Item 
-                                        asChild
-                                        value="sign-out"
-                                        fontSize="lg"
-                                        fontWeight="semibold"
-                                    >
-                                        <Link
-                                            variant="plain"
-                                            textDecoration="none"
-                                            onClick={handleSignOutClick}
-                                        >
-                                        Sign Out
-                                        </Link>
-                                    </Menu.Item>
-                                </Menu.Content>
-                            </Menu.Positioner>
-                        </Portal>
-                    </Menu.Root>
-                </>
-            )}
-        </HStack>
-    )}
-</>
-*/
     );
 }
